ed607a1e0a46343efad6921ef898241bed71ca5d
malinhodev <malinhodev@gmail.com>
2025-05-13 17:13:21 -0300

    mudando icones da seção de sites

--- js/sites.js
+++ js/sites.js
@@ -9,27 +9,27 @@ document.querySelectorAll('[data-toggle="open-sites"]').forEach( o => {
                                  '</div>'+
                                  '<div id="contentModal">'+
                                  '<div class="card">'+
-                                     '<a href="https://malinhodev.github.io/CloneNetflix/" target="_blank"><ion-icon name="tv-outline"></ion-icon>'+
+                                     '<a href="https://malinhodev.github.io/CloneNetflix/" target="_blank"><ion-icon name="browsers-outline"></ion-icon>'+
                                      '<h3>Clone netflix</h3></a>'+
                                  '</div><!--card-->'+
                                  '<div class="card">'+
-                                     '<a href="https://malinhodev.github.io/rocketnews/" target="_blank"><ion-icon name="tv-outline"></ion-icon>'+
+                                     '<a href="https://malinhodev.github.io/rocketnews/" target="_blank"><ion-icon name="browsers-outline"></ion-icon>'+
                                      '<h3>Rocketnews</h3></a>'+
                                  '</div><!--card-->'+
                                  '<div class="card">'+
-                                     '<a href="https://malinhodev.github.io/whatsappwebclone/" target="_blank"><ion-icon name="tv-outline"></ion-icon>'+
+                                     '<a href="https://malinhodev.github.io/whatsappwebclone/" target="_blank"><ion-icon name="browsers-outline"></ion-icon>'+
                                      '<h3>Whatsapp webclone</h3></a>'+
                                  '</div><!--card-->'+
                                  '<div class="card">'+
-                                     '<a href="https://espacoluanaaguiar.c68.com.br" target="_blank"><ion-icon name="tv-outline"></ion-icon>'+
+                                     '<a href="https://espacoluanaaguiar.c68.com.br" target="_blank"><ion-icon name="browsers-outline"></ion-icon>'+
                                      '<h3>Espaço Luana Aguiar</h3></a>'+
                                  '</div><!--card-->'+
                                  '<div class="card">'+
-                                     '<a href="https://manoscell.c68.com.br" target="_blank"><ion-icon name="tv-outline"></ion-icon>'+
+                                     '<a href="https://manoscell.c68.com.br" target="_blank"><ion-icon name="browsers-outline"></ion-icon>'+
                                      '<h3>Manos Cell</h3></a>'+
                                  '</div><!--card-->'+
                                  '<div class="card">'+
-                                     '<a href="https://agenciacode68.com" target="_blank"><ion-icon name="tv-outline"></ion-icon>'+
+                                     '<a href="https://agenciacode68.com" target="_blank"><ion-icon name="browsers-outline"></ion-icon>'+
                                      '<h3>Agência Code 68</h3></a>'+
                                  '</div><!--card-->'+
                                  '</div>'+      
